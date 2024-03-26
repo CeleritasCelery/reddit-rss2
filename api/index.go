5930f9ba4e15219d64eb7ebfe2fb17951fca3f7b
J.J. Schwarzbart <schwarzbartj@gmail.com>
2024-03-26 17:35:08 +0100

    change - use forked version of go-reddit

--- api/index.go
+++ api/index.go
@@ -4,10 +4,11 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/trashhalo/reddit-rss/pkg/client"
+	"github.com/jojubart/reddit-rss/pkg/client"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	httpClient := http.DefaultClient
 	client.RssHandler("https://old.reddit.com", time.Now, httpClient, client.GetArticle, w, r)
 }
+
